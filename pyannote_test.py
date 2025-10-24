d1af91c7f4b5a21c0c56e85cdc3ae1293f04d1cb
Gonzalo Gamez <takosaga@gmail.com>
2025-10-24 17:30:03 +0300

    feat: pyannote working

--- pyannote_test.py
+++ pyannote_test.py
@@ -3,9 +3,17 @@ import os
 from dotenv import load_dotenv
 from pyannote.audio import Pipeline
 from pyannote.audio.pipelines.utils.hook import ProgressHook
+from pathlib import Path
 
 load_dotenv()
 HUGGINGFACE_ACCESS_TOKEN = os.getenv('HUGGINGFACE_ACCESS_TOKEN')
+
+# Get Downloads directory
+downloads_path = Path.home() / "Downloads"
+
+# Access specific .wav file
+file_path = downloads_path / "test.wav" 
+
 # Community-1 open-source speaker diarization pipeline
 pipeline = Pipeline.from_pretrained(
     "pyannote/speaker-diarization-community-1",
@@ -13,3 +21,11 @@ pipeline = Pipeline.from_pretrained(
 
 # send pipeline to GPU (when available)
 pipeline.to(torch.device("cuda"))
+
+# apply pretrained pipeline (with optional progress hook)
+with ProgressHook() as hook:
+    output = pipeline(file_path, hook=hook)  # runs locally
+
+# print the result
+for turn, speaker in output.speaker_diarization:
+    print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
