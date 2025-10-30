31493f6abd7a75be3e9bf58f88e977aa63ca0ff9
Gonzalo Gamez <takosaga@gmail.com>
2025-10-30 17:13:47 +0200

    feat: adjusting to video folder

--- pyannote_test.py
+++ pyannote_test.py
@@ -8,12 +8,6 @@ from pathlib import Path
 load_dotenv()
 HUGGINGFACE_ACCESS_TOKEN = os.getenv('HUGGINGFACE_ACCESS_TOKEN')
 
-# Get Downloads directory
-downloads_path = Path.home() / "Downloads"
-
-# Access specific .wav file
-file_path = downloads_path / "output.wav" 
-
 # Community-1 open-source speaker diarization pipeline
 pipeline = Pipeline.from_pretrained(
     "pyannote/speaker-diarization-community-1",
@@ -24,7 +18,7 @@ pipeline.to(torch.device("cuda"))
 
 # apply pretrained pipeline (with optional progress hook)
 with ProgressHook() as hook:
-    output = pipeline(file_path, hook=hook)  # runs locally
+    output = pipeline('videos/test.wav', hook=hook)  # runs locally
 
 # print the result
 for turn, speaker in output.speaker_diarization:
